1f78139693abed15bacee2297059ac172a4325a4
Abhinav N <155301793+abhisavage@users.noreply.github.com>
2024-08-08 21:25:50 +0530

    Add files via upload

---
+++ utilz/LoadSave.java
@@ -0,0 +1,70 @@
+package utilz;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+import main.Game;
+
+public class LoadSave {
+
+	public static final String PLAYER_ATLAS = "player_sprites.png";
+	public static final String LEVEL_ATLAS = "outside_sprites.png";
+	public static final String LEVEL_ONE_DATA = "level_one_data.png";
+
+	/*public static BufferedImage GetSpriteAtlas(String fileName) {
+		BufferedImage img = null;
+		InputStream is = LoadSave.class.getResourceAsStream("/" + fileName);
+		try {
+			img = ImageIO.read(is);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return img;
+	}*/
+
+	public static BufferedImage GetSpriteAtlas(String fileName) {
+		BufferedImage img = null;
+	
+		try {
+			ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+			URL resourceURL = classLoader.getResource("res/" + fileName);
+			if (resourceURL != null) {
+				img = ImageIO.read(resourceURL);
+			} else {
+				System.out.println("Resource not found: " + fileName);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+		return img;
+	}
+	
+	public static int[][] GetLevelData() {
+		int[][] lvlData = new int[Game.TILES_IN_HEIGHT][Game.TILES_IN_WIDTH];
+		BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
+
+		for (int j = 0; j < img.getHeight(); j++)
+			for (int i = 0; i < img.getWidth(); i++) {
+				Color color = new Color(img.getRGB(i, j));
+				int value = color.getRed();
+				if (value >= 48)
+					value = 0;
+				lvlData[j][i] = value;
+			}
+		return lvlData;
+
+	}
+}
